e9b4f9c8fb64a5c33b12b123f8f8829806db14a3
the_annoying _ <abhay.gupta220@gmail.com>
2021-06-24 20:31:24 +0530

    3 Testimonials manually shown

--- covidui/src/views/Testimonials.vue
+++ covidui/src/views/Testimonials.vue
@@ -1,18 +1,59 @@
 <template>
   <div>
         <div class="box">
-        <div class="corousel">
-            <div class="columns">
-                <div class="column userImg">
-                    <img src="https://www.afro.who.int/sites/default/files/2021-02/COVID-19%20survivor%20supporting%20sensitization%20activities%20in%20Lagos%20i.jpg">
-                </div>
-                <div class="column">
-                    <p>{{ currentU.name }}</p>
-                    <p>{{ currentT.exp }}</p>
-                    <p> {{ currentT.suggest }}</p>
-                </div>
+            <div>
+                <vue-corousel :arrows="true" :autoplay="true" :autoplaySpeed="5000">
+                    <div class="corousel">
+                            <div class="userImg">
+                                <img class="image" src="https://s.yimg.com/ny/api/res/1.2/SCKEa8wrINtD1dZr7FWbuw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtjZj13ZWJw/https://s.yimg.com/os/creatr-uploaded-images/2021-06/e1932a80-d171-11eb-afbc-3c0898217567" style="padding: 0px">
+                            </div>
+                            <div class="content">
+                                <p class="subtitle"><strong>My covid-19 experience: </strong></p>
+                                <p>Tripti, a 28-year old RJ from Lucknow got infected with Covid-19 around the end of March 2021. She surmised that the virus got infected from her father. She narrated the incident too. One fine day, her father wakes up with a fever, and post-check up it was found that he was tested positive.</p>
+                                <p>“We were shit scared, because he’s almost 60 years old and he has diabetes too. By the time his symptoms started showing, we were together, doing chores for him, I kinda knew that I was in close contact with my father. Although his symptoms were mild like fever, fatigue, he was home-isolated, but after 2-3 days I had fever, runny nose, and fatigue are some of the starting signs of my coronavirus” says Tripti on how she was infected with the virus and her initial symptoms.</p>
+                                <p>Five days into the fever, later, she wasn't able to drink water. She wasn't able to sit for 2-3 seconds. Being a chaiholic, she didn’t even feel like drinking chai (tea). Her close friend used to drop medicines, fruits and veggies. “I was drained out completely. My father recovered by that time, where he followed everything religiously like taking steam, ayurvedic medicines (giloy and ashwagandha tablets for immunity), but I was worsely affected and started dehydrated”, states RJ.</p>
+                                <p>During her first two weeks of covid-recovery, she had faced a lot of distress with her health. “My face became pale, I lost weight, my lips dried, my eyes popped out, and I got skinny within those 7-8 days”, added Tripti.</p>
+                                <p>We asked about the feeling when she was caught with the Covid-19, she said, “That was the lowest point of my life. I can still feel that feeling. I was so “weak”. No energy to even get up and sit. I could not even open my eyes. I was lying down.” She further added, “I remember it was 2 or 3 at night. I thought I was gone. I messaged my close friend about it. Now that I joke about it, laugh about it, but that was a low moment.”<p>
+                                Tripti’s recovery took 3-4 weeks. She shared how she recovered from the virus. “Sleep is very necessary. The more you sleep, the more you get better. There was a ray of hope I was hung on to. My family kept me sane and they supported me in every way.” Her friend used to get the electoral sachets and her brother helped her in completing drinking it. “Mann na bhi ho, finish it in 3-4 hours”, she quoted her brother. She got better by following the routine for 4-5 days.
+                                During her recovery, she consumed several fruits like pomegranate, watermelon, and all the foods that have water content. The worst part of her recovery was loss of smell and taste. “It took almost a month for me to get it back and start tasting. I was scared that I would forget how things used to taste. I still doubt that it’s not the taste that I used to taste, things used to taste, before I had COVID” asserts the Lucknow RJ. Slowly and gradually, the sense of smell got better, “but it doesn’t feel the same though” says Tripti.</p>
+                                <p class="subtitle"><strong>What I suggest to current patients: </strong></p>
+                                <p>You can always rely on your family for support. For those people who are, whoever is there in a situation, their family members are affected or contacted with virus, please be there. Your support means everything, more than the medicines, more than the diet, more than the doctor’s treatment. That will make a difference.</p>
+                            </div>
+                    </div>
+                    <div class="corousel">
+                            <div class="userImg">
+                                <img class="image" src="https://s.yimg.com/ny/api/res/1.2/F519HbhP3xMTHchufJiCNA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtjZj13ZWJw/https://s.yimg.com/os/creatr-uploaded-images/2021-06/91e8e1b0-d058-11eb-afb1-1113b654c9fd" style="padding: 0px">
+                            </div>
+                            <div class="content">
+                                <p class="subtitle"><strong>My covid-19 experience: </strong></p>
+                                <p>Tripti, a 28-year old RJ from Lucknow got infected with Covid-19 around the end of March 2021. She surmised that the virus got infected from her father. She narrated the incident too. One fine day, her father wakes up with a fever, and post-check up it was found that he was tested positive.</p>
+                                <p>“We were shit scared, because he’s almost 60 years old and he has diabetes too. By the time his symptoms started showing, we were together, doing chores for him, I kinda knew that I was in close contact with my father. Although his symptoms were mild like fever, fatigue, he was home-isolated, but after 2-3 days I had fever, runny nose, and fatigue are some of the starting signs of my coronavirus” says Tripti on how she was infected with the virus and her initial symptoms.</p>
+                                <p>Five days into the fever, later, she wasn't able to drink water. She wasn't able to sit for 2-3 seconds. Being a chaiholic, she didn’t even feel like drinking chai (tea). Her close friend used to drop medicines, fruits and veggies. “I was drained out completely. My father recovered by that time, where he followed everything religiously like taking steam, ayurvedic medicines (giloy and ashwagandha tablets for immunity), but I was worsely affected and started dehydrated”, states RJ.</p>
+                                <p>During her first two weeks of covid-recovery, she had faced a lot of distress with her health. “My face became pale, I lost weight, my lips dried, my eyes popped out, and I got skinny within those 7-8 days”, added Tripti.</p>
+                                <p>We asked about the feeling when she was caught with the Covid-19, she said, “That was the lowest point of my life. I can still feel that feeling. I was so “weak”. No energy to even get up and sit. I could not even open my eyes. I was lying down.” She further added, “I remember it was 2 or 3 at night. I thought I was gone. I messaged my close friend about it. Now that I joke about it, laugh about it, but that was a low moment.”<p>
+                                Tripti’s recovery took 3-4 weeks. She shared how she recovered from the virus. “Sleep is very necessary. The more you sleep, the more you get better. There was a ray of hope I was hung on to. My family kept me sane and they supported me in every way.” Her friend used to get the electoral sachets and her brother helped her in completing drinking it. “Mann na bhi ho, finish it in 3-4 hours”, she quoted her brother. She got better by following the routine for 4-5 days.
+                                During her recovery, she consumed several fruits like pomegranate, watermelon, and all the foods that have water content. The worst part of her recovery was loss of smell and taste. “It took almost a month for me to get it back and start tasting. I was scared that I would forget how things used to taste. I still doubt that it’s not the taste that I used to taste, things used to taste, before I had COVID” asserts the Lucknow RJ. Slowly and gradually, the sense of smell got better, “but it doesn’t feel the same though” says Tripti.</p>
+                                <p class="subtitle"><strong>What I suggest to current patients: </strong></p>
+                                <p>You can always rely on your family for support. For those people who are, whoever is there in a situation, their family members are affected or contacted with virus, please be there. Your support means everything, more than the medicines, more than the diet, more than the doctor’s treatment. That will make a difference.</p>
+                            </div>
+                    </div>
+                    <div class="corousel">
+                            <div class="userImg">
+                                <img class="image" src="https://s.yimg.com/ny/api/res/1.2/cibsLn_GmJx0JaByBib5rA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtjZj13ZWJw/https://s.yimg.com/os/creatr-uploaded-images/2021-06/1bba4040-d172-11eb-bbcf-a5d67f102a69" style="padding: 0px">
+                            </div>
+                            <div class="content">
+                                <p class="subtitle"><strong>My covid-19 experience: </strong></p>
+                                <p>Ayandrali Dutta, a 42-year old, hails from Odisha, but now she’s residing in Noida. She’s a journalist by profession and a Guest faculty at NIFT. She was affected by Covid-19 around 20th May 2021. It all started with a slight itch in her throat. She used to get fever only during nights. She had almost lost her voice along with too much coughing and could barely talk. So, without any further thought, she took the meds. On the advice of a doctor, she got a few tests done and later got it reviewed. “Thankfully it didn’t cost me a bomb but it was somewhat around INR 8000-9000/-, including the tests.</p>
+                                <p>Thankfully, her situation didn’t get worse, but the pandemic is such a horrendous thing that not only affects your inside, but also takes away your calmness within you. So was the condition of Ayandrila: “I must tell you that this virus kills you both emotionally and physically. The body felt terribly lethargic and I didn’t even have the energy to make a cup of tea. The pain of not having anyone close to you is just something beyond words”.</p>
+                                <p>On the road to recovery, she lost 5.5 kgs of weight. Her friends and well-wishers kept on checking on her by sending meals. “There were friends who would drop meds, cash or anything that I needed and would leave. This whole process in itself seemed pretty disheartening. I had totally stopped any news site or even watching any reels that were around the same. I held on to my hope and positivity and trust me music really helps”, she added.</p>
+                                <p>However, she kept on checking her O2 levels on a regular basis. “When I was finally tested negative, it almost felt like I had won a big battle. I have gone back to doing what I love the most- cooking and now I cook and send meals to those who need it the most. The chain of kindness need not break”, asserts Ayandrila.</p>
+                                <p>Presuming that life gave a second-chance to live to those who recovered, when asked the same, Ayandrilla replied: “I surely know today that second chances happen and one needs to embrace them. I have become extra careful with hygiene and sanitation and even make sure that I eat right. I have kinda decided to keep my meals mostly homemade and surely focus on something called savings”<p>
+                                <p class="subtitle"><strong>What I suggest to current patients: </strong></p>
+                                <p>Don't forget to share your feelings with people. Be honest with them. But make sure these are people who help you, challenge you, and who don’t let you sit in it. Appreciate small things around you, and a little “shukr” and a lot of “sabr” is what we all need.</p>
+                            </div>
+                    </div>
+                </vue-corousel>
             </div>
-        </div>
         </div>
         <footer class="footer">
             <p>Have you also recovered from Covid-19 ? </p>
@@ -49,7 +90,7 @@
                     <button class="button is-success" @click="save()">Share</button>
                     </footer>
                 </div>
-            </div>
+        </div>
   </div>
 </template>
 
@@ -58,11 +99,15 @@ import { getCookie } from '../main'
 import DatePicker from 'vue2-datepicker';
 import 'vue2-datepicker/index.css';
 import axios from 'axios';
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
 export default {
     name: "Testimonials",
     components : {
-        'date-picker': DatePicker
+        'date-picker': DatePicker,
+        'vue-corousel': VueSlickCarousel
     },
     data: function () {
         return {
@@ -73,25 +118,12 @@ export default {
                 suggest: ""
             },
             show: false,
-            index: 0,
-            currentT: {},
             users: {},
-            currentU: {}
         }
     },
     computed: {
         uid: function () {
             return getCookie('uid') || '';
-        }
-    },
-    watch: {
-        index: function () {
-            if (this.index>=5) {
-                this.index = 0;
-            }
-            this.currentT = this.allTestimonials[this.index]
-            var id = this.currentT.user.$oid;
-            this.currentU = this.users[id];
         }
     },
     methods: {
@@ -117,7 +149,6 @@ export default {
     created: function () {
         axios.get('/api/v1/testimonials').then(resp => {
             this.allTestimonials = resp.data.data;
-            this.currentT = this.allTestimonials[0];
             (this.allTestimonials).forEach((tm)=> {
                 var id = tm.user.$oid || ''
                 if (id !== '' && !this.users[id]){
@@ -125,24 +156,11 @@ export default {
                         if (r.data) {
                             var u = r.data.data.data;
                             this.users[id] = u;
-                            if (id === this.currentT.uid){
-                                this.currentU = u;
-                            }
                         }
                     })
                 }
             })
         })
-    },
-    mounted: function () {
-            this.index = 0;
-            this.currentT = this.allTestimonials[this.index];
-            var id = this.currentT.user.$oid;
-            this.currentU = this.users[id];
-            setInterval(function () {
-                console.log("called");
-                this.index = this.index + 1;
-            }, 1000);
     }
 }
 </script>
@@ -154,6 +172,7 @@ export default {
     margin: auto;
     margin-top: 2%;
     padding: 0px;
+    margin-bottom: 2%;
 }
 
 .add {
@@ -176,14 +195,33 @@ export default {
 }
 
 .userImg {
-    margin-top: 0px;
-    margin-bottom: 0px;
+    text-align: -webkit-center;
+    margin: auto;
+    margin-bottom: 1%;
 }
 
 .footer {
-    position: absolute;
     bottom: 0px;
     width: 100%;
 }
 
+.corousel {
+    padding: 1%;
+    max-height: 1000px;
+    overflow: auto;
+}
+
+.image {
+    height: 300px !important;
+    width: 600px !important;
+}
+
+@media screen and (max-width: 950px) {
+    .box {
+        width: 90%;
+    }
+    .modal-card {
+        width: 90%;
+    }
+}
 </style>
