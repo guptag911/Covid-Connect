891c61547090d9f0a0535fef871db584ae906fa7
the_annoying _ <abhay.gupta220@gmail.com>
2021-07-01 14:13:20 +0530

    Hosted on domain

--- covidConnect/covidConnect/settings.py
+++ covidConnect/covidConnect/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://covidconnectui.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://covidconnectui.herokuapp.com/', 'www.covidconnect.co.in']
 
 CORS_ORIGIN_ALLOW_ALL=True
 
